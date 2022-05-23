5d89c55027bbba63c5c159e43a0e9695ebd1bd57
Prohor722 <prohor.b722@gmail.com>
2022-05-23 19:08:39 +0600

    mongo connect and get products api created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,30 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.rk7zy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(process.env.USER_NAME);
+
+async function run(){
+    try{
+        await client.connect();
+
+        const gearBikesCollection = client.db("gearBikes").collection("products");
+        const usersCollection = client.db("gearBikes").collection("users");
+        const reviewsCollection = client.db("gearBikes").collection("reviews");
+
+        //get all products
+        app.get('/products',(req,res)=>{
+            const products = gearBikesCollection.find().toArray();
+            res.send(products)
+        })
+    }
+    finally{
+    }
+}
+
+run().catch().dir
 
 app.get('/',(req,res)=>{
     res.send("Welcome to GearBikes");
